14ca153b67d8f850572ece0e625f7f5102bdf4c6
Jack Hong <jackhong6@users.noreply.github.com>
2025-08-16 22:19:34 -0700

    Updated templates

--- test-exercises.R
+++ test-exercises.R
@@ -1,28 +1,17 @@
 library("exams")
 
 # Include all the questions in the kinematics folder in the exam.
-my_exam <- dir(path = "exercises/elasticity")
+my_exam <- dir(path = "exercises/kinematics")
 my_exam <- sample(my_exam, 45) # randomize
 
 
 # Include only the exercises listed.
-my_exam <- list("calculate-d-given-vi-vf-t-aircraft-carrier.Rmd",
-    "calculate-a-given-0-to-60.Rmd",
-    "free-fall-vertical-launch-max-height-scaling-with-vi.Rmd",
-    "free-fall-diff-in-distances.Rmd",
-    "free-fall-diff-in-speeds.Rmd",
-    "free-fall-compare-impact-speed-thrown-up-vs-down.Rmd",
-    "calculate-t-given-vi-projectile-1d.Rmd",
-                "calculate-marathon-time-given-pace.Rmd",
-                "calculate-t-given-a-vi-vf-kmh.Rmd",
-                "stopping-distance-scaling-with-speed.Rmd",
-                "calculate-t-given-d-v.Rmd",
-                "calculate-t-given-a-vi-vf.Rmd")
+my_exam <- list("calculate-tension-angled-beam-with-load.Rmd")
 
 # Generate different PDFs
-exams2pdf(my_exam, edir = "exercises")
-exams2pdf(my_exam, edir = "exercises", template = "solution.tex")
-exams2nops(my_exam, edir = "exercises")
+exams2pdf(my_exam, edir = "exercises", template = "templates/practice", header = list(Course = "Physics 12", Title = "Statics Practice Problems"))
+exams2pdf(my_exam, edir = "exercises", template = "templates/solution.tex")
+exams2nops(my_exam, edir = "exercises", institution = "McRoberts Secondary", logo = "graphics/school-logo.png", blank = 0, samepage = TRUE)
 
 # Stress test an exercise
 st_results <- stresstest_exercise("C:/Users/jackh/OneDrive/Documents/GitHub/R-exams-physics-problems/exercises/statics/calculate-balance-position-on-massless-seesaw.Rmd")
