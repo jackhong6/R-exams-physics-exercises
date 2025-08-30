71b3c67a22ec2640caf3f59b770b384876eebd57
Jack Hong <jackhong6@users.noreply.github.com>
2025-08-30 10:07:32 -0700

    Added more statics exercises

--- test-exercises.R
+++ test-exercises.R
@@ -1,18 +1,18 @@
 library("exams")
 
 # Include all the questions in the specified folder in the exam.
-my_exam <- dir(path = "exercises/kinematics")
+my_exam <- dir(path = "exercises/statics")
 my_exam <- sample(my_exam, 45) # randomize
 
 
 # Include only the exercises listed.
-my_exam <- list("compare-range-at-different-angles.Rmd", "calculate-range-2d-projectile.Rmd", "range-on-moon.Rmd", "range-on-planet.Rmd")
+my_exam <- list("forces-on-cantilever.Rmd")
 
 # Generate different PDFs
 exams2pdf(my_exam, edir = "exercises", template = "templates/practice", 
           header = list(Course = "Physics 12", Title = "Statics Practice Problems"))
 exams2pdf(my_exam, edir = "exercises", template = "templates/solution.tex", 
-          header = list(Course = "Physics 11", Title = "Solutions to Kinematics Practice Problems"))
+          header = list(Course = "Physics 12", Title = "Solutions to Statics Practice Problems"))
 exams2nops(my_exam, edir = "exercises", institution = "McRoberts Secondary", logo = "graphics/school-logo.png", blank = 0, samepage = TRUE)
 
 # Stress test an exercise
