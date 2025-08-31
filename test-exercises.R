b71892e2e07525fcbd0cb1df18d0d7ad99b323d8
Jack Hong <jackhong6@users.noreply.github.com>
2025-08-30 21:50:51 -0700

    Added more statics problems

--- test-exercises.R
+++ test-exercises.R
@@ -6,7 +6,7 @@ my_exam <- sample(my_exam, 45) # randomize
 
 
 # Include only the exercises listed.
-my_exam <- list("forces-on-cantilever.Rmd")
+my_exam <- list("calculate-tension-in-hanging-scaffold.Rmd")
 
 # Generate different PDFs
 exams2pdf(my_exam, edir = "exercises", template = "templates/practice", 
@@ -16,6 +16,6 @@ exams2pdf(my_exam, edir = "exercises", template = "templates/solution.tex",
 exams2nops(my_exam, edir = "exercises", institution = "McRoberts Secondary", logo = "graphics/school-logo.png", blank = 0, samepage = TRUE)
 
 # Stress test an exercise
-st_results <- stresstest_exercise("~/Documents/GitHub/R-exams-physics-exercises/exercises/kinematics/calculate-range-2d-projectile.Rmd")
+st_results <- stresstest_exercise("~/Documents/GitHub/R-exams-physics-exercises/exercises/statics/calculate-tension-in-hanging-scaffold.Rmd")
 plot(st_results)
 
