6659933c6a85b0f18f6c68b9bb19f5f391928532
Jack Hong <jackhong6@users.noreply.github.com>
2025-09-07 18:17:29 -0700

    Added sample exams

--- test-exercises.R
+++ test-exercises.R
@@ -1,8 +1,8 @@
 library("exams")
 
 # Include all the questions in the specified folder in the exam.
-my_exam <- dir(path = "exercises/statics")
-my_exam <- sample(my_exam, 6) # randomize
+my_exam <- c(dir("exercises/kinematics"), dir("exercises/statics"), dir("exercises/elasticity"))
+my_exam <- sample(my_exam, 45) # randomize
 
 
 # Include only the exercises listed.
@@ -21,9 +21,16 @@ st_results <- stresstest_exercise("~/Documents/GitHub/R-exams-physics-exercises/
 plot(st_results)
 
 set.seed(1)
-exams2nops(my_exam, edir = "exercises", title = "Statics Exam", course = "Physics 12", institution = "McRoberts Secondary", logo = "graphics/school-logo.png", 
-           blank = 0, samepage = TRUE, usepackage = c("siunitx"))
+exams2nops(my_exam, edir = "exercises", title = "Sample Exam 1", course = "Physics", institution = "McRoberts Secondary", logo = "graphics/school-logo.png", 
+           blank = 0, samepage = TRUE, dir = "output", name = "sample-exam-1", usepackage = c("siunitx"))
 set.seed(1)
-exams2pdf(my_exam, edir = "exercises", template = "templates/solution.tex", 
-          header = list(Course = "Physics 12", Title = "Solutions to Statics Exam"))
+exams2pdf(my_exam, edir = "exercises", template = "templates/solution.tex", header = list(Course = "Physics", Title = "Solutions to Sample Exam 1"),
+          dir = "output", name = "sample-exam-1-solutions")
+
+set.seed(2)
+exams2nops(my_exam, edir = "exercises", title = "Sample Exam 2", course = "Physics", institution = "McRoberts Secondary", logo = "graphics/school-logo.png", 
+           blank = 0, samepage = TRUE, dir = "output", name = "sample-exam-2", usepackage = c("siunitx"))
+set.seed(2)
+exams2pdf(my_exam, edir = "exercises", template = "templates/solution.tex", header = list(Course = "Physics", Title = "Solutions to Sample Exam 2"),
+          dir = "output", name = "sample-exam-2-solutions")
 
