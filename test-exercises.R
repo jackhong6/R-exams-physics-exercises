eb68f82b7682a6d892fbd891326339ef9e96bba2
Jack Hong <jackhong6@users.noreply.github.com>
2025-09-07 17:51:27 -0700

    More statics exercises

--- test-exercises.R
+++ test-exercises.R
@@ -2,20 +2,28 @@ library("exams")
 
 # Include all the questions in the specified folder in the exam.
 my_exam <- dir(path = "exercises/statics")
-my_exam <- sample(my_exam, 45) # randomize
+my_exam <- sample(my_exam, 6) # randomize
 
 
 # Include only the exercises listed.
-my_exam <- list("calculate-stress.Rmd", "calculate-piano-wire-tension.Rmd")
+my_exam <- list("calculate-min-angle-ladder.Rmd")
 
 # Generate different PDFs
 exams2pdf(my_exam, edir = "exercises", template = "templates/practice", 
           header = list(Course = "Physics 12", Title = "Statics Practice Problems"))
 exams2pdf(my_exam, edir = "exercises", template = "templates/solution.tex", 
           header = list(Course = "Physics 12", Title = "Solutions to Statics Practice Problems"))
-exams2nops(my_exam, edir = "exercises", institution = "McRoberts Secondary", logo = "graphics/school-logo.png", blank = 0, samepage = TRUE)
+exams2nops(my_exam, edir = "exercises", institution = "McRoberts Secondary", logo = "graphics/school-logo.png", blank = 0, samepage = TRUE,
+           usepackage = c("siunitx"))
 
 # Stress test an exercise
 st_results <- stresstest_exercise("~/Documents/GitHub/R-exams-physics-exercises/exercises/statics/calculate-tension-in-hanging-scaffold.Rmd")
 plot(st_results)
+
+set.seed(1)
+exams2nops(my_exam, edir = "exercises", title = "Statics Exam", course = "Physics 12", institution = "McRoberts Secondary", logo = "graphics/school-logo.png", 
+           blank = 0, samepage = TRUE, usepackage = c("siunitx"))
+set.seed(1)
+exams2pdf(my_exam, edir = "exercises", template = "templates/solution.tex", 
+          header = list(Course = "Physics 12", Title = "Solutions to Statics Exam"))
 
